14d1271ff9280d3e2751d108962572523365980c
Sergio Miranda <granrah1@gmail.com>
2024-12-04 01:35:20 -0300

    ahora si ??

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -16,7 +16,10 @@ export const corsConfig: CorsOptions = {
         if(process.argv[2] === '--api'){
             whitelist.push(undefined);
         }
-        if(whitelist.includes(origin)){
+
+        console.log(whitelist);
+        console.log('origin -> ', origin);
+        if(whitelist.includes(origin || process.env.BACKEND_URL! || process.env.FRONTEND_URL!)){
             callback(null, true);
         } else {
             callback(new Error('Error de CORS'));
